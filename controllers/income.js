52d726ae8f8a4c431992fe44914ba8e7209655c6
okoid721 <okoid721@gmail.com>
2024-03-24 23:52:54 +0100

    eccomerce

--- controllers/income.js
+++ controllers/income.js
@@ -32,5 +32,20 @@ exports.addIncome = async (req, res) => {
 exports.getIncomes = async (req, res) => {
   try {
     const incomes = await IncomeSchema.find().sort({ createdAt: -1 });
-  } catch (error) {}
+    res.status(200).json(incomes);
+  } catch (error) {
+    console.log('error in getIncomes:', error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+exports.deleteIncome = async (req, res) => {
+  const { id } = req.params;
+  IncomeSchema.findByIdAndDelete(id)
+    .then((income) => {
+      res.status(200).json({ message: 'Income deleted successfully!' });
+    })
+    .catch((error) => {
+      console.log('error deleting income:', error);
+      res.status(500).json({ message: 'Server Error' });
+    });
 };
