c3a23914f8bd20c884433417be6aea1105350c47
okoid721 <okoid721@gmail.com>
2024-03-24 22:53:48 +0100

    eccomerce

--- controllers/income.js
+++ controllers/income.js
@@ -1,3 +1,14 @@
+const IncomeSchema = require('../models/IncomeModel');
 exports.addIncome = async (req, res) => {
+  const { title, amount, category, description, date } = req.body;
+
+  const income = IncomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
   console.log(req.body);
+  console.log(income);
 };
